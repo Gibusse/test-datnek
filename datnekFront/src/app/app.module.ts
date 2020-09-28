bb13d25a367fbb873e417aaff89006f241de2d82
Coolbreeze <maugerpierre@live.com>
2020-09-28 12:36:16 +0100

    added login form

--- datnekFront/src/app/app.module.ts
+++ datnekFront/src/app/app.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- datnekFront/src/app/login/login.component.ts
+++ datnekFront/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
+
+  initForm() {
+    this.loginForm = this.fb.group({
+      userEmail: ['']
+    })
+  }
+
 
 }
