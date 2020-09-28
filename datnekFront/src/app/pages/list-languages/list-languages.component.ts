015ce56bca03070f58e9dbcf102d7ee8daba5d71
Coolbreeze <maugerpierre@live.com>
2020-09-28 15:49:53 +0100

    added table view on list of languages from Material

--- datnekFront/src/app/pages/list-languages/list-languages.component.ts
+++ datnekFront/src/app/pages/list-languages/list-languages.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {ListLanguages} from "../../models/list-languages";
+import {MatPaginator} from "@angular/material/paginator";
+
+const ELEMENT_DATA: ListLanguages[] = [
+  {id: 1, language: 'français', writing: 'intermédiaire', comprehension: 'courant', speaking: 'intermédiaire'},
+  {id: 2, language: 'anglais', writing: 'intermédiaire', comprehension: 'courant', speaking: 'intermédiaire'},
+  {id: 3, language: 'nerlandais', writing: 'intermédiaire', comprehension: 'courant', speaking: 'intermédiaire'}
+]
 
 @Component({
   selector: 'app-list-languages',
   templateUrl: './list-languages.component.html',
   styleUrls: ['./list-languages.component.scss']
 })
-export class ListLanguagesComponent implements OnInit {
+export class ListLanguagesComponent implements OnInit, AfterViewInit {
 
-  displayedColumns: string[] = []
+  displayedColumns: string[] = ['language', 'writing', 'speaking', 'comprehension', 'id'];
+  dataSource = new MatTableDataSource<ListLanguages>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
--- datnekFront/src/app/pages/pages.module.ts
+++ datnekFront/src/app/pages/pages.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {COMPONENTS, PagesRoutingModule} from "./pages-routing.module";
 import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatIconModule} from "@angular/material/icon";
 
 const components = [
   ...COMPONENTS
@@ -14,7 +16,9 @@ const components = [
   imports: [
     CommonModule,
     PagesRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatPaginatorModule,
+    MatIconModule
   ]
 })
 export class PagesModule { }
