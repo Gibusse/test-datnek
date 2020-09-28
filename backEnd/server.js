8d5830e3f5ddc7ff82cbf46cb90173541ae2f70a
Coolbreeze <maugerpierre@live.com>
2020-09-28 14:17:52 +0100

    added api route file

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+
 const app = express();
 
 const PORT = 3000;
+const api = require('./routes/api')
 
 // Middleware
 app.use(bodyParser.json({urlencoded: true}))
-
+app.use('/api', api);
 
 
 app.get('/', (req, res) => {
