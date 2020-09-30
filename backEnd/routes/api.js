e12dbb461b49565d808ffc2ccbe26388c0c00909
Coolbreeze <maugerpierre@live.com>
2020-09-30 19:02:09 +0100

    added service to get languages from dband parameted route

--- backEnd/routes/api.js
+++ backEnd/routes/api.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const selectedLanguages = require('../services/selectedServices');
 const user = require('../services/userServices');
+const languages = require('../services/languageServices');
 
 /**
  *  Show that the url is on localhost:3000/api
@@ -29,6 +30,10 @@ router.get('/selectedLanguage', (req, res) => {
 router.post('/login', (req, res) =>{
     let email = req.body;
     user.register(email, res);
+});
+
+router.get('/languages', (req, res) => {
+    languages.getAllLanguages(res);
 })
 
 module.exports = router
--- backEnd/services/languageServices.js
+++ backEnd/services/languageServices.js
@@ -1,2 +1,17 @@
 const mysql = require('../config/MySQL');
 
+
+
+module.exports.getAllLanguages = function(res) {
+    const querySelect = 'SELECT * FROM languages';
+
+    mysql.db.query(querySelect, (err, result) => {
+        if (err) {
+            res.status(400).send(err)
+        } else {
+            result.message = 'Ajout réussi';
+            res.status(200).send(result)
+        }
+
+    });
+}
